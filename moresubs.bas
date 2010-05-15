rversteegen/ohrrpgce

commit 015976e259ddb15e0beb4c1140be5dce6a01bb8b

Fix a script interpreter crash that's been plaguing my "1 day in #castleparadox" game: actually I expected this bug was there, because I never retested the script freeing mechanism while optimising the interpreter. Also a few misc minor improvements here and there
git-svn-id: svn://gilgamesh.hamsterrepublic.com/ohrrpgce/wip@2708 7d344553-34f0-0310-a9b1-970ce8f1c3a2

moresubs.bas
@@ -1655,10 +1655,10 @@ FUNCTION loadscript (n as integer) as integer
 
   IF skip >= 12 THEN
    GET #f, 9, .strtable
-   .strtable = (.strtable - skip) \ wordsize
+   IF .strtable THEN .strtable = (.strtable - skip) \ wordsize
   ELSEIF skip = 10 THEN
    GET #f, 9, temp
-   .strtable = (temp - skip) \ wordsize
+   IF .strtable THEN .strtable = (temp - skip) \ wordsize
   ELSE
    .strtable = 0
   END IF
@@ -1666,13 +1666,13 @@ FUNCTION loadscript (n as integer) as integer
   'set an arbitrary max script buffer size (scriptmemMax in const.bi), individual scripts must also obey
   .size = (LOF(f) - skip) \ wordsize
   IF .size > scriptmemMax THEN
-   scripterr "Script " & n & " exceeds maximum size"
+   scripterr "Script " & n & " exceeds maximum size by " & .size * 100 \ scriptmemMax - 99 & "%"
    CLOSE #f
    RETURN -1
   END IF
 
   IF .size + totalscrmem > scriptmemMax OR numloadedscr = 128 THEN
-   'debug "opps! size = " & .size & " totalscrmem = " & totalscrmem & ", calling freescripts"
+   'debug "loadscript(" & n & " '" & scriptname(n) & "'): scriptbuf full; size = " & .size & " totalscrmem = " & totalscrmem & ", calling freescripts"
    freescripts(scriptmemMax - .size)
   END IF
 
@@ -1713,8 +1713,8 @@ FUNCTION loadscript (n as integer) as integer
 END FUNCTION
 
 SUB freescripts (mem)
-'frees loaded scripts until totalscrmem <= mem (measured in 4-byte ints) (probably alot lower)
-'always frees at least one script to provide an opening in script()
+'frees loaded scripts until at least totalscrmem <= mem (measured in 4-byte ints) (probably alot lower)
+'also makes sure there are at least 8 unused slots in script()
 'call freescripts(0) to cleanup all scripts
 
 'give each script a score (the lower, the more likely to throw) and sort them
@@ -1725,7 +1725,9 @@ listtail = -1
 FOR i = 0 TO 127
  WITH script(i)
   IF .id THEN
-   score = .lastuse + iif(.totaluse < 200, .totaluse, 200) - .size \ 256
+   'this formula is having only been given the most minimal of testing for suitability
+   score = .lastuse - scriptctr
+   score = iif(score > -150, score, -150) + iif(.totaluse < 150, .totaluse, 150) - .size \ (scriptmemMax \ 256)
    FOR j = listtail TO 0 STEP -1
     IF score >= LRUlist(j, 1) THEN EXIT FOR
     LRUlist(j + 1, 0) = LRUlist(j, 0)
@@ -1751,7 +1753,7 @@ NEXT
 ' END WITH
 'NEXT
 
-
+'aim for at most 75% of memory limit
 targetmem = mem
 IF mem > scriptmemMax \ 2 THEN targetmem = mem * (1 - 0.5 * (mem - scriptmemMax \ 2) / scriptmemMax)
 
@@ -1759,16 +1761,21 @@ IF mem > scriptmemMax \ 2 THEN targetmem = mem * (1 - 0.5 * (mem - scriptmemMax 
 
 FOR i = 0 TO listtail
  WITH script(LRUlist(i, 0))
+  IF totalscrmem <= targetmem AND numloadedscr + 8 < UBOUND(script) THEN EXIT SUB
+
+  'debug "deallocating " & .id & " " & scriptname(.id) & " size " & .size
   totalscrmem -= .size
   numloadedscr -= 1
   deallocate(.ptr)
   .id = 0
   IF .refcount THEN
    FOR j = 0 TO nowscript
-    IF scrat(j).scrnum = LRUlist(i, 0) THEN scrat(j).scrnum = -1
+    IF scrat(j).scrnum = LRUlist(i, 0) THEN
+     'debug "marking scrat(" & j & ") (id = " & scrat(j).id & ") unloaded"
+     scrat(j).scrnum = -1
+    END IF
    NEXT
   END IF
-  IF totalscrmem <= targetmem THEN EXIT SUB
  END WITH
 NEXT
 

const.bi
@@ -125,7 +125,7 @@ CONST max_npc_defs = 99
 CONST maxMaxItems = 32000 'max number of items
 CONST maxMaxHero = 59 'This is the max value possible for gen(genMaxHero) 'FIXME: not used everywhere
 CONST inventoryMax = 599 'last inventory slot num (divisible by 3 when you count the zero)
-CONST scriptmemMax = 65536    'in 4-byte ints (256kb)
+CONST scriptmemMax = 65536 'in 4-byte ints (256kb)
 
 '--- Binary files in BINSIZE.BIN for getbinsize()
 CONST binATTACK = 0

game.bas
@@ -128,6 +128,10 @@ DIM plotstr(31) as Plotstring
 DIM scrst as Stack
 DIM curcmd as ScriptCommand ptr
 
+'incredibly frustratingly fbc doesn't export global array debugging symbols
+DIM as ScriptInst ptr scratp = @scrat(0)
+DIM as ScriptData ptr scriptp = @script(0)
+
 'End global variables
 
 'DEBUG debug "Thestart"

misc/gdbscripts
@@ -18,7 +18,7 @@ define pel
 	while 1
 
 		if $dims < 1 || $dims > 5 || $data[1] == 0
-			echo Array descriptor invalid
+			echo Array descriptor invalid\n
 			loop_break
 		end
 
@@ -30,7 +30,7 @@ define pel
 			set $dimno += 1
 		end
 		if &((char*)$arg0)[$elem * $data[3]] != &((char*)$data[1])[$check * $data[3]]
-			echo Array descriptor invalid
+			echo Array descriptor invalid\n
 			loop_break
 		end
 
@@ -50,8 +50,8 @@ document pel
 	INDICES is an array of indices. Make sure you get the length
 	right, as there's no way for the script to check
 	Eg.
-	DIM array(300, 3) as MyType
-	(gdb) pel ARRAY {20, 1} MYTYPE
+	 DIM array(300, 3) as MyType
+	 gdb> pel ARRAY {20, 1} MYTYPE
 end
 
 define parray
@@ -81,7 +81,7 @@ define parray
 
 	while 1
 		if $dims < 1 || $dims > 5 || $data[1] == 0
-			echo Array descriptor invalid
+			echo Array descriptor invalid\n
 			loop_break
 		end
 
@@ -110,7 +110,7 @@ define parray
 		echo ):\n
 
 		if (char*)$data[1] != ((char*)$data[0]) + ($check * $data[3])
-			echo Array descriptor invalid
+			echo Array descriptor invalid\n
 			loop_break
 		end
 
@@ -172,7 +172,7 @@ document parray
 	necessary for large integer arrays.
 	Eg.
 	DIM array(300, 3) as integer
-	(gdb) parry ARRAY int 1
+	gdb> parray ARRAY int 1
 end
 
 define runcatch

yetmore.bas
@@ -2507,7 +2507,7 @@ IF mode > 1 AND viewmode = 0 THEN
 END IF
 
 IF mode > 1 AND viewmode = 1 THEN
- reloadscript scrat(selectedscript)
+ reloadscript scrat(selectedscript), 0
  WITH scrat(selectedscript)
   IF script(.scrnum).vars = 0 THEN
    edgeprint "Has no variables", 0, ol, uilook(uiText), page
@@ -2677,7 +2677,7 @@ END IF
 
 'just incase was swapped out above
 IF nowscript >= 0 THEN
- reloadscript scrat(nowscript)
+ reloadscript scrat(nowscript), 0
 END IF
 END SUB
 

yetmore2.bas
@@ -919,6 +919,7 @@ SUB reloadscript (si as ScriptInst, updatestats)
  IF si.scrnum = -1 THEN
   si.scrnum = loadscript(si.id)
   IF si.scrnum = -1 THEN killallscripts: EXIT SUB
+  si.scrdata = script(si.scrnum).ptr
   script(si.scrnum).refcount += 1
   IF updatestats THEN script(si.scrnum).totaluse += 1
  END IF

commit 94c3e92dd037cc92280f4c4a28ae0999241dad72

I noticed a typo (introduced in r2708: "Also a few misc minor improvements here and there") which broke string literals in games made with r803 to r1008 (late 2006)
git-svn-id: svn://gilgamesh.HamsterRepublic.com/ohrrpgce/wip@3545 7d344553-34f0-0310-a9b1-970ce8f1c3a2

moresubs.bas
@@ -1360,7 +1360,7 @@ FUNCTION loadscript (n as unsigned integer) as ScriptData ptr
    IF .strtable THEN .strtable = (.strtable - skip) \ wordsize
   ELSEIF skip = 10 THEN
    GET #f, 9, temp
-   IF .strtable THEN .strtable = (temp - skip) \ wordsize
+   IF temp THEN .strtable = (temp - skip) \ wordsize
   ELSE
    .strtable = 0
   END IF