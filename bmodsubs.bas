rversteegen/ohrrpgce

commit 4c7ebdaed2a287252214699958f7ea8f7af33b93

Cleanup and rename smartarrows() SUB to battle_target_arrows()
git-svn-id: svn://gilgamesh.HamsterRepublic.com/ohrrpgce/wip@3746 7d344553-34f0-0310-a9b1-970ce8f1c3a2

bmodsubs.bas
@@ -635,53 +635,55 @@ SUB setbatcap (BYREF bat AS BattleState, cap as string, captime as integer, capd
  bat.caption_delay = capdelay
 END SUB
 
-SUB smartarrowmask (inrange() as integer, d as integer, axis as integer, bslot() AS BattleSprite, targ AS TargettingState)
-FOR i = 0 TO 11
- IF targ.mask(i) THEN
-  IF axis THEN
-   distance = (bslot(i).y - bslot(targ.pointer).y) * d
-  ELSE
-   distance = (bslot(i).x - bslot(targ.pointer).x) * d
-  END IF
-  IF distance > 0 THEN
-   setbit inrange(), 0, i, 1
-  END IF
- END IF
-NEXT i
-END SUB
+OPTION EXPLICIT 'FIXME: move this up as code gets cleaned up
 
-SUB smartarrows (d as integer, axis as integer, bslot() AS BattleSprite, BYREF targ AS TargettingState, allow_spread as integer=0)
-DIM inrange(0)
-inrange(0) = 0
-smartarrowmask inrange(), d, axis, bslot(), targ
-IF inrange(0) THEN
- best = 999
- newptr = targ.pointer
- FOR i = 0 TO 11
-  IF readbit(inrange(), 0, i) THEN
+SUB battle_target_arrows_mask (inrange() as integer, d as integer, axis as integer, bslot() AS BattleSprite, targ AS TargettingState)
+ DIM distance AS INTEGER
+ FOR i AS INTEGER = 0 TO 11
+  IF targ.mask(i) THEN
    IF axis THEN
     distance = (bslot(i).y - bslot(targ.pointer).y) * d
    ELSE
-    distance = (bslot(i).x - bslot(targ.pointer).y) * d
+    distance = (bslot(i).x - bslot(targ.pointer).x) * d
    END IF
-   IF distance < best THEN
-    best = distance
-    newptr = i
+   IF distance > 0 THEN
+    setbit inrange(), 0, i, 1
    END IF
   END IF
  NEXT i
- targ.pointer = newptr
-ELSE
- IF allow_spread = YES AND targ.opt_spread = 1 THEN
-  FOR i = 0 TO 11
-   targ.selected(i) = targ.mask(i)
-  NEXT i
-  targ.opt_spread = 2
- END IF
-END IF
 END SUB
 
-OPTION EXPLICIT 'FIXME: move this up as code gets cleaned up
+SUB battle_target_arrows (d as integer, axis as integer, bslot() AS BattleSprite, BYREF targ AS TargettingState, allow_spread as integer=0)
+ DIM inrange(0)
+ inrange(0) = 0
+ battle_target_arrows_mask inrange(), d, axis, bslot(), targ
+ IF inrange(0) THEN
+  DIM best AS INTEGER = 999
+  DIM newptr AS INTEGER = targ.pointer
+  DIM distance AS INTEGER
+  FOR i AS INTEGER = 0 TO 11
+   IF readbit(inrange(), 0, i) THEN
+    IF axis THEN
+     distance = (bslot(i).y - bslot(targ.pointer).y) * d
+    ELSE
+     distance = (bslot(i).x - bslot(targ.pointer).y) * d
+    END IF
+    IF distance < best THEN
+     best = distance
+     newptr = i
+    END IF
+   END IF
+  NEXT i
+  targ.pointer = newptr
+ ELSE
+  IF allow_spread = YES AND targ.opt_spread = 1 THEN
+   FOR i AS INTEGER = 0 TO 11
+    targ.selected(i) = targ.mask(i)
+   NEXT i
+   targ.opt_spread = 2
+  END IF
+ END IF
+END SUB
 
 FUNCTION targetmaskcount (tmask() as integer) as integer
  DIM n AS INTEGER = 0

bmod.bas
@@ -771,16 +771,16 @@ SUB battle_targetting(BYREF bat AS BattleState, bslot() AS BattleSprite)
  'arrow keys to select
  IF bat.targ.interactive = YES AND bat.targ.opt_spread < 2 AND bat.targ.roulette = NO AND bat.targ.force_first = NO THEN
   IF carray(ccUp) > 1 THEN
-   smartarrows -1, 1, bslot(), bat.targ, NO
+   battle_target_arrows -1, 1, bslot(), bat.targ, NO
   END IF
   IF carray(ccDown) > 1 THEN
-   smartarrows 1, 1, bslot(), bat.targ, NO
+   battle_target_arrows 1, 1, bslot(), bat.targ, NO
   END IF
   IF carray(ccLeft) > 1 THEN
-   smartarrows -1, 0, bslot(), bat.targ, YES
+   battle_target_arrows -1, 0, bslot(), bat.targ, YES
   END IF
   IF carray(ccRight) > 1 THEN
-   smartarrows 1, 0, bslot(), bat.targ, YES
+   battle_target_arrows 1, 0, bslot(), bat.targ, YES
   END IF
  END IF
  

bmodsubs.bi
@@ -36,8 +36,8 @@ declare sub anim_retreat (who as integer, attack as AttackData, bslot() AS Battl
 declare function safesubtract (number as integer, minus as integer) as integer
 declare function safemultiply (number as integer, by as single) as integer
 declare sub setbatcap (BYREF bat AS BattleState, cap as string, captime as integer, capdelay as integer)
-declare sub smartarrowmask (inrange() as integer, d as integer, axis as integer, bslot() as battlesprite, targ AS TargettingState)
-declare sub smartarrows (d as integer, axis as integer, bslot() as battlesprite, BYREF targ AS TargettingState, allow_spread as integer=0)
+declare sub battle_target_arrows_mask (inrange() as integer, d as integer, axis as integer, bslot() as battlesprite, targ AS TargettingState)
+declare sub battle_target_arrows (d as integer, axis as integer, bslot() as battlesprite, BYREF targ AS TargettingState, allow_spread as integer=0)
 declare function targetmaskcount (tmask() as integer) as integer
 declare sub traceshow (s as string)
 declare function trytheft (BYREF bat AS BattleState, who as integer, targ as integer, attack as AttackData, bslot() as BattleSprite) as integer

commit 23fb95f205ca64b4255d88dfd4044eb7ab1369bd

Fix battle cursor movement bug when pressing left/right keys introduced in r3746
git-svn-id: svn://gilgamesh.HamsterRepublic.com/ohrrpgce/wip@4480 7d344553-34f0-0310-a9b1-970ce8f1c3a2

bmodsubs.bas
@@ -689,7 +689,7 @@ SUB battle_target_arrows (d as integer, axis as integer, bslot() AS BattleSprite
     IF axis THEN
      distance = (bslot(i).y - bslot(targ.pointer).y) * d
     ELSE
-     distance = (bslot(i).x - bslot(targ.pointer).y) * d
+     distance = (bslot(i).x - bslot(targ.pointer).x) * d
     END IF
     IF distance < best THEN
      best = distance