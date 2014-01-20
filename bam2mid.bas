rversteegen/ohrrpgce

commit ce408b5224418f127933687b0bfb8401dcf1d731

Fix gengcc=1 with FB 0.91
I replaced a call to malloc with ALLOCATE since for some reason FB was generating
two declarations of malloc (this is normal: one is for malloc, the other for
ALLOCATE), but one of them with an 'integer' (C typedef) type instead of
'uinteger'. Which is very strange because both crt/stdlib.bi and the fbc internal
function type table use unsigned arguments. (No the fix to cutil.bi didn't
fix it.)

git-svn-id: svn://gilgamesh.hamsterrepublic.com/ohrrpgce/wip@6320 7d344553-34f0-0310-a9b1-970ce8f1c3a2

bam2mid.bas
@@ -12,6 +12,7 @@
 ''
 
 #include "banks.bi"
+#include "crt/stddef.bi"
 
 #define VELOCITY 		96
 
@@ -19,7 +20,7 @@
 #define NOTE_ON		&b10010000
 #define PATCH_CHANGE 	&b11000000
 
-declare function fput alias "fb_FilePut" ( byval fnum as integer, byval pos as uinteger = 0, byval src as any ptr, byval bytes as integer ) as integer
+declare function fput alias "fb_FilePut" ( byval fnum as long, byval pos as long = 0, byval src as any ptr, byval bytes as size_t ) as long
 
 declare sub setbigval(byval value as integer)
 declare sub setsmallval(byval value as integer)

util.bi
@@ -17,6 +17,7 @@ CONST YES = -1
 CONST NO = 0
 
 #include "vector.bi"
+#include "crt/stddef.bi"
 
 
 '----------------------------------------------------------------------
@@ -319,9 +320,9 @@ declare sub set_tmpdir ()
 ' leading to conflicts with -gen gcc, so use the fbc prototype)
 'declare function fget alias "fb_FileGet" ( byval fnum as integer, byval pos as integer = 0, byval dst as any ptr, byval bytes as uinteger ) as integer
 'declare function fput alias "fb_FilePut" ( byval fnum as integer, byval pos as integer = 0, byval src as any ptr, byval bytes as uinteger ) as integer
-declare function fget alias "fb_FileGet" ( byval fnum as integer, byval pos as uinteger = 0, byval dst as any ptr, byval bytes as integer ) as integer
-declare function fput alias "fb_FilePut" ( byval fnum as integer, byval pos as uinteger = 0, byval src as any ptr, byval bytes as integer ) as integer
-declare function fgetiob alias "fb_FileGetIOB" ( byval fnum as integer, byval pos as integer = 0, byval dst as any ptr, byval bytes as uinteger, byval bytesread as uinteger ptr ) as integer
+declare function fget alias "fb_FileGet" ( byval fnum as long, byval pos as long = 0, byval dst as any ptr, byval bytes as size_t ) as long
+declare function fput alias "fb_FilePut" ( byval fnum as long, byval pos as long = 0, byval src as any ptr, byval bytes as size_t ) as long
+declare function fgetiob alias "fb_FileGetIOB" ( byval fnum as long, byval pos as long = 0, byval dst as any ptr, byval bytes as size_t, byval bytesread as size_t ptr ) as long
 
 
 '----------------------------------------------------------------------

cutil.bi
@@ -5,10 +5,8 @@
 
 extern "C"
 
-#ifndef size_t
-type size_t as integer
-#endif
-
+' For size_t
+#include "crt/stddef.bi"
 
 'In miscc.c
 declare function get_sys_err_string() as zstring ptr

util.bas
@@ -288,7 +288,7 @@ END FUNCTION
 
 'Returns the number of characters at the start of two strings that are equal
 FUNCTION length_matching(s1 as string, s2 as string) as integer
- DIM as ubyte ptr p1 = @s1[0], p2 = @s2[0]
+ DIM as byte ptr p1 = @s1[0], p2 = @s2[0]
  DIM as integer ret = 0
  WHILE *p1 AND *p2
   IF *p1 <> *p2 THEN RETURN ret
@@ -865,7 +865,7 @@ SUB remove_string_cache (cache() as IntStrPair, byval key as integer)
 END SUB
 
 FUNCTION strhash(hstr as string) as unsigned integer
- RETURN stringhash(cptr(ubyte ptr, strptr(hstr)), len(hstr))
+ RETURN stringhash(cptr(zstring ptr, strptr(hstr)), len(hstr))
 END FUNCTION
 
 
@@ -885,13 +885,13 @@ FUNCTION hash_file(filename as string) as unsigned integer
   hash += hash SHL 8
   DIM buf(4095) as ubyte
   WHILE size > 0
-    DIM readamnt as integer
+    DIM readamnt as uinteger
     fgetiob fh, , @buf(0), 4096, @readamnt
     IF readamnt < size AND readamnt <> 4096 THEN
       debug "hash_file: fgetiob failed!"
       RETURN 0
     END IF
-    hash xor= stringhash(@buf(0), readamnt)
+    hash xor= stringhash(cptr(zstring ptr, @buf(0)), readamnt)
     hash += ROT(hash, 5)
     size -= 4096
   WEND
@@ -1181,7 +1181,7 @@ END FUNCTION
 
 FUNCTION escape_filenamec CDECL (byval filename as zstring ptr) as zstring ptr
   DIM ret as string = escape_filename(*filename)
-  DIM retz as zstring ptr = malloc(LEN(ret) + 1)
+  DIM retz as zstring ptr = ALLOCATE(LEN(ret) + 1)
   strcpy retz, cstring(ret)
   RETURN retz
 END FUNCTION

vector.bi
@@ -30,9 +30,15 @@
 #DEFINE vector ptr
 
 TYPE FnCtor as sub cdecl (byval as any ptr)
-TYPE FnCompare as function cdecl (byval as any ptr, byval as any ptr) as integer
 TYPE FnCopy as sub cdecl (byval as any ptr, byval as any ptr)
 TYPE FnStr as function cdecl (byval as any ptr) as string
+#IF __FB_VERSION__ < "0.91"
+ 'I don't know why, but using 'long' instead of 'integer' on 32 bit FB versions < 0.91
+ 'generates a warning even they're the same size
+ TYPE FnCompare as function cdecl (byval as any ptr, byval as any ptr) as integer
+#ELSE
+ TYPE FnCompare as function cdecl (byval as any ptr, byval as any ptr) as long
+#ENDIF
 
 'Not used
 ENUM PassConvention

commit 34e5d0fa8e05c47c42c646d4682fd9cf5349ed7b

Fix FB 0.90 gengcc=1 (inc. Android builds) compile error caused by r6320
git-svn-id: svn://gilgamesh.hamsterrepublic.com/ohrrpgce/wip@6339 7d344553-34f0-0310-a9b1-970ce8f1c3a2

bam2mid.bas
@@ -20,7 +20,11 @@
 #define NOTE_ON		&b10010000
 #define PATCH_CHANGE 	&b11000000
 
+#IF __FB_VERSION__ < "0.91"
+declare function fput alias "fb_FilePut" ( byval fnum as integer, byval pos as uinteger = 0, byval src as any ptr, byval bytes as integer ) as integer
+#ELSE
 declare function fput alias "fb_FilePut" ( byval fnum as long, byval pos as long = 0, byval src as any ptr, byval bytes as size_t ) as long
+#ENDIF
 
 declare sub setbigval(byval value as integer)
 declare sub setsmallval(byval value as integer)

util.bi
@@ -338,10 +338,14 @@ declare sub set_tmpdir ()
 'slight hackery to get more versatile read function
 '(fbc internally defines these functions differently from their actual prototypes,
 ' leading to conflicts with -gen gcc, so use the fbc prototype)
-'declare function fget alias "fb_FileGet" ( byval fnum as integer, byval pos as integer = 0, byval dst as any ptr, byval bytes as uinteger ) as integer
-'declare function fput alias "fb_FilePut" ( byval fnum as integer, byval pos as integer = 0, byval src as any ptr, byval bytes as uinteger ) as integer
+
+#IF __FB_VERSION__ < "0.91"
+declare function fget alias "fb_FileGet" ( byval fnum as integer, byval pos as uinteger = 0, byval dst as any ptr, byval bytes as integer ) as integer
+declare function fput alias "fb_FilePut" ( byval fnum as integer, byval pos as uinteger = 0, byval src as any ptr, byval bytes as integer ) as integer
+#ELSE
 declare function fget alias "fb_FileGet" ( byval fnum as long, byval pos as long = 0, byval dst as any ptr, byval bytes as size_t ) as long
 declare function fput alias "fb_FilePut" ( byval fnum as long, byval pos as long = 0, byval src as any ptr, byval bytes as size_t ) as long
+#ENDIF
 declare function fgetiob alias "fb_FileGetIOB" ( byval fnum as long, byval pos as long = 0, byval dst as any ptr, byval bytes as size_t, byval bytesread as size_t ptr ) as long
 
 