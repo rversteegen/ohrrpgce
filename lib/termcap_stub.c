35f6bae6a066390d1f900372c1997551cb63663b
Ralph Versteegen <teeemcee@gmail.com>
2022-07-20 15:19:14 +1200

    WIP extend termcap_stub.c

--- lib/termcap_stub.c
+++ lib/termcap_stub.c
@@ -14,17 +14,34 @@ char * UP;
 char * BC;
 NCURSES_OSPEED ospeed;
 
+#define BUFSZ 10
+char buf[BUFSZ];
+
 NCURSES_EXPORT(char *) tgetstr (const char *id, char **area) {
+    if (strcmp(id, "ce") == 0)  // SEQ_CLEOL - clear until end of line
+        return "\e[K";
+    if (strcmp(id, "cm") == 0)  // SEQ_LOCATE - move cursor
+        return "cm";
+    if (strcmp(id, "ve") == 0)  // SEQ_SHOW_CURSOR - make cursor visible
+        return "\e[?25h";
+    if (strcmp(id, "vi") == 0)  // SEQ_HIDE_CURSOR - make cursor invisible
+        return "\e[?25l";
+
     return 0;
 }
 NCURSES_EXPORT(char *) tgoto (const char *cap, int col, int row) {
-    return 0;
+    if (strcmp(cap, "cm") == 0) {
+//        snprintf(buf, BUFSZ, "\e[%d;%df", row, col);
+        snprintf(buf, BUFSZ, "\e[%d;%dH", row, col);
+        return buf;
+    }
+    return (char *)cap;
 }
 NCURSES_EXPORT(int) tgetent (char *bp, const char *name) {
     return 1;
 }
 NCURSES_EXPORT(int) tgetflag (const char *id) {
-    if (memcmp(id, "am", 2) == 0)
+    if (strcmp(id, "am") == 0)
         return 1;
     return 0;
 }
